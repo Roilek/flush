47082973d2d1bded9bf120b79e2b4b04780462d4
Roilek <elior.papiernik@gmail.com>
2023-01-07 17:43:36 +0100

    Add base code for the telegram bot to setup for Heroku

---
+++ main.py
@@ -0,0 +1,74 @@
+# Create a telegram bot and host it on Heroku
+
+import os
+import logging
+from dotenv import load_dotenv
+
+from telegram import Update
+from telegram.ext import CommandHandler, Application, ConversationHandler, MessageHandler, filters, CallbackContext
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+load_dotenv()
+
+# Constants
+
+TOKEN = os.environ.get('TOKEN')
+PORT = int(os.getenv('PORT', 5000))
+HEROKU_PATH = os.getenv('HEROKU_PATH')
+
+
+# Define a few command handlers. These usually take the two arguments update and
+# context. Error handlers also receive the raised TelegramError object in error.
+async def start(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /start is issued."""
+    await update.message.reply_text('Hi!')
+    return
+
+
+async def help_command(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /help is issued."""
+    await update.message.reply_text('Help!')
+    return
+
+
+async def echo(update: Update, context: CallbackContext) -> None:
+    """Echo the user message."""
+    await update.message.reply_text(update.message.text)
+    return
+
+
+async def error(update: Update, context: CallbackContext) -> None:
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+    return
+
+
+def main() -> None:
+    """Start the bot."""
+    print("Going live!")
+
+    # Create application
+    application = Application.builder().token(TOKEN).build()
+
+    # on different commands - answer in Telegram
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("help", help_command))
+
+    # Start the Bot
+    print("Bot starting...")
+    if os.environ.get('ENV') == 'DEV':
+        application.run_polling()
+    elif os.environ.get('ENV') == 'PROD':
+        application.run_webhook(listen="0.0.0.0",
+                                port=int(PORT),
+                                webhook_url=HEROKU_PATH)
+    return
+
+
+if __name__ == '__main__':
+    main()
